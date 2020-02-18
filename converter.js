2d4e153ceab0402560102b8262bfca6ffab0e6eb
Tyler Ruotolo <tylerruotolotech@gmail.com>
2020-02-18 12:55:47 -0500

    update 1.1

--- converter.js
+++ converter.js
@@ -1,6 +1,9 @@
+var blaze = document.getElementById("blaze");
+var usd = document.getElementById("usd");
+var tweet = document.getElementById('twitter-share-button');
+
 function convert(){
-    var blaze = document.getElementById("blaze");
-    var usd = document.getElementById("usd");
     var divide = 500;
     usd.value = "$" + blaze.value/divide; 
+    tweet.href = "https://twitter.com/intent/tweet?text=" + "I made " + usd.value + " on @storyfireapp in Blaze. I found out using blazetousd.tk!";
 }
