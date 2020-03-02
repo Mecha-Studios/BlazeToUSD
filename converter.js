e9e028f1990ef28b940453d00718974ee77cbbf6
Tyler Ruotolo <tylerruotolotech@gmail.com>
2020-03-02 07:59:15 -0500

    1.5

--- converter.js
+++ converter.js
@@ -1,13 +1,14 @@
 var blaze = document.getElementById("blaze");
 var usd = document.getElementById("usd");
 var tweet = document.getElementById('twitter-share-button');
-var version = 1.4;
+var version = 1.5;
 
 console.log("BlazeToUSD V" + version);
 
 
 function convert(){
     var divide = 500;
-    usd.value = "$" + blaze.value/divide; 
-    tweet.href = "https://twitter.com/intent/tweet?text=" + "I made " + usd.value + " on @storyfireapp in Blaze. I found out using blazetousd.tk!";
+    let finalAmount = blaze.value/divide;
+    usd.value = "$" + Math.floor(finalAmount * 100) / 100;
+    tweet.href = "https://twitter.com/intent/tweet?text=" + "I made " + usd.value + " on @storyfireapp in Blaze. I found out using blazetousd.tk! Click here if you'd like to donate to help fund this project: paypal.me/tylerruotolo";
 }
