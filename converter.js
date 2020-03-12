873ebc77c094c78379952779e98afbc6ac0b5b31
Tyler Ruotolo <tylerruotolotech@gmail.com>
2020-03-12 12:50:09 -0400

    BlazeToUSD 2.0

--- converter.js
+++ converter.js
@@ -1,8 +1,9 @@
 var blaze = document.getElementById("blaze");
 var usd = document.getElementById("usd");
 var tweet = document.getElementById('twitter-share-button');
-var version = "1.6.1";
+var version = "1.7";
 var versiontext = document.getElementById("versionnumber");
+var views = document.getElementById("views");
 
 console.log("BlazeToUSD Version:" + version);
 versiontext.innerHTML+= version;
@@ -12,4 +13,11 @@ function convertUSD(){
     let finalAmount = blaze.value/divide;
     usd.value = "$" + Math.floor(finalAmount * 100) / 100;
     tweet.href = "https://twitter.com/intent/tweet?text=" + "I made " + usd.value + " on @storyfireapp in Blaze. I found out using blazetousd.tk! Click here if you'd like to donate to help fund this project: paypal.me/tylerruotolo";
+}
+
+function convertViews(){
+    var divideblaze = 500;
+    let blazeandviews = blaze.value*views.value/divideblaze;
+    usd.value = "$" + Math.floor(final * 100) / 100;
+
 }
